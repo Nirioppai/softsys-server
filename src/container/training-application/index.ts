619d2fb461450c908791b3dc0dd2bd2ce1c23395
@FreshMaker-L <johnlouienorme@yahoo.com>
2021-06-15 19:11:20 +0800

    Added CRUD Apis for Training-Application Module

--- src/container/training-application/index.ts
+++ src/container/training-application/index.ts
@@ -1,5 +1,6 @@
 import ApplicationModel from './model';
 import ApplicationController from './controller';
 import ApplicationService from './service';
+import { validateCreate, validateUpdate } from './validate';
 
-export { ApplicationController, ApplicationModel, ApplicationService };
+export { ApplicationController, ApplicationModel, ApplicationService, validateCreate, validateUpdate };
--- src/container/training-application/routers.ts
+++ src/container/training-application/routers.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import ApplicationController from './index';
+import { ApplicationController, validateCreate, validateUpdate } from './index';
 import { jwtAuth, checkIfAdmin, checkAccess } from '../../_common/check-token';
 
 const router = express.Router();
@@ -10,10 +10,52 @@ const router = express.Router();
  */
 
 /**
- * Route to get all administrator
+ * Route to GET All Training Application
  * @param { token, type } req
  *
  */
 router.get('/get-all', [jwtAuth], ApplicationController.getAllApplication);
+
+/**
+ * Route to GET All Training Application
+ * @param { token, type } req
+ *
+ */
+router.get('/get/:id', [jwtAuth], ApplicationController.getOneApplication);
+
+/**
+ * Route to CREATE One Training Application
+ * @param { token, type } req
+ *
+ */
+router.post('/createOne', [jwtAuth], ApplicationController.createOneApplication);
+
+/**
+ * Route to CREATE Many Training Applications
+ * @param { token, type } req
+ *
+ */
+router.post('/createMany', [jwtAuth], ApplicationController.createManyApplication);
+
+/**
+ * Route to UPDATE One Training Application
+ * @param { token, type } req
+ *
+ */
+router.put('/update/:id', [jwtAuth], ApplicationController.updateApplication);
+
+/**
+ * Route to DELETE One Training Application
+ * @param { token, type } req
+ *
+ */
+router.delete('/deleteOne/:id', [jwtAuth], ApplicationController.deleteOneApplication);
+
+/**
+ * Route to DELETE Many Training Application
+ * @param { token, type } req
+ *
+ */
+router.delete('/deleteMany', [jwtAuth], ApplicationController.deleteManyApplications);
 
 export = router;
