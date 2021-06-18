3de5affb746e5edf7db6338fc59a94ca61a93102
jon roido <jlgt2200@gmail.com>
2021-06-19 06:38:17 +0800

    employee controllers, services, routers and employee rest

--- src/container/employee/controller.ts
+++ src/container/employee/controller.ts
@@ -5,10 +5,32 @@ import EmployeeService from './service';
 import { sendResponse } from '../../_common/response';
 
 const employeeService = new EmployeeService();
-// get all admin accounts
-const getAllEmployee = async (req: Request, res: Response) => {
-    let result: object = await employeeService.getAllEmployees();
+// get all employee
+const getAll = async (req: Request, res: Response) => {
+    let result: object = await employeeService.getAll();
+    sendResponse(res, result);
+};
+// get employee
+const getOne = async (req: Request, res: Response) => {
+    let result: object = await employeeService.getOne(req.params.id);
+    sendResponse(res, result);
+};
+// update employee
+const updateOne = async (req: Request, res: Response) => {
+    let result: object = await employeeService.updateOne(req.params.id, req.body);
+    sendResponse(res, result);
+};
+// delete employee
+const deleteOne = async (req: Request, res: Response) => {
+    const _id: string = req.params.id;
+    let result: object = await employeeService.deleteOne(_id);
     sendResponse(res, result);
 };
 
-export default { getAllEmployee };
+// delete Many employee
+const deleteMany = async (req: Request, res: Response) => {
+    let body = req.body.items;
+    let result: object = await employeeService.deleteMany(body);
+    sendResponse(res, result);
+};
+export default { getAll, getOne, updateOne, deleteOne, deleteMany };
