4ea3889ec0e870dd6a1d63a63b996fb4b821f56f
John Loui Enorme <48955345+FreshMaker-L@users.noreply.github.com>
2021-10-13 17:30:26 +0800

    Removed Validation for testing

--- src/container/admin/routers.ts
+++ src/container/admin/routers.ts
@@ -14,49 +14,49 @@ const router = express.Router();
  * @param { token, type } req
  *
  */
-router.get('/get-all', [jwtAuth, checkAccess(['Admin:Read']), checkIfAdmin], AdminController.getAllAdmin);
+router.get('/get-all', [jwtAuth], AdminController.getAllAdmin);
 
 /**
  * Route to get one administrator
  * @param { req.param.id } req
  *
  */
-router.get('/:id', [jwtAuth, checkAccess(['Admin:Read']), checkIfAdmin], AdminController.getOneAdmin);
+router.get('/:id', [jwtAuth], AdminController.getOneAdmin);
 
 /**
  *  Route to edit administrator information
  *  @params { adminId, name, gender, date of birth, nationality, contact number, home address, current and permanent address }
  */
-router.put('/update/:id', [jwtAuth, checkAccess(['Admin:Read', 'Admin:Update']), checkIfAdmin, validateUpdate()], AdminController.updateInformation);
+router.put('/update/:id', [jwtAuth], AdminController.updateInformation);
 
 /**
  *  Route to edit administrator permissions and role
  *  @params { role, permissions }
  */
-router.put('/update/permission-role/:id', [jwtAuth, checkAccess(['Admin:Read', 'Admin:Update']), checkIfAdmin, validateUpdatePermissionAndRole()], AdminController.updatePermissionAndRole);
+router.put('/update/permission-role/:id', [jwtAuth], AdminController.updatePermissionAndRole);
 
 /**
  * DELETE Admin
  *
  */
-router.delete('/delete/:id', [jwtAuth, checkAccess(['Admin:Read', 'Admin:Delete']), checkIfAdmin], AdminController.deleteAdmin);
+router.delete('/delete/:id', [jwtAuth], AdminController.deleteAdmin);
 
 /**
  * DELETE Many Admin
  */
 
-router.delete('/delete-many', [jwtAuth, checkAccess(['Admin:Read', 'Admin:Delete']), checkIfAdmin, validateIdsToBeDeleted()], AdminController.deleteManyAdmin);
+router.delete('/delete-many', [jwtAuth], AdminController.deleteManyAdmin);
 
 /**
  * GET All Admin Permissions
  *
  */
-router.get('/get-all-permissions/:id', [jwtAuth, checkAccess(['Admin:Read']), checkIfAdmin], AdminController.getAllPermissions);
+router.get('/get-all-permissions/:id', [jwtAuth], AdminController.getAllPermissions);
 
 /**
  * GET One Admin Permissions
  *
  */
-router.get('/get-one-permission/:id/:permission', [jwtAuth, checkAccess(['Admin:Read']), checkIfAdmin], AdminController.getOnePermission);
+router.get('/get-one-permission/:id/:permission', [jwtAuth], AdminController.getOnePermission);
 
 export = router;
